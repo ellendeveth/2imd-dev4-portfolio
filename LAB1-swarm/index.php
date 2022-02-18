3b22c94953b6515be7c637e949528ee146355cf1
Ellen <ellen.deveth@hotmail.com>
2022-02-18 11:24:49 +0100

    feat: add files

---
+++ LAB1-swarm/index.php
@@ -0,0 +1,55 @@
+<?php
+
+    $array = [
+        [
+            "image" => "",
+            "username" => "Jesse",
+            "place" => "Assembly 3.0",
+            "location" => "San Francisco, CA"
+        ],
+        [
+            "image" => "",
+            "username" => "Jesse",
+            "place" => "Assembly 3.0",
+            "location" => "San Francisco, CA"
+        ],
+        [
+            "image" => "",
+            "username" => "Jesse",
+            "place" => "Assembly 3.0",
+            "location" => "San Francisco, CA"
+        ],
+        [
+            "image" => "",
+            "username" => "Jesse",
+            "place" => "Assembly 3.0",
+            "location" => "San Francisco, CA"
+        ],
+    ]
+    /*
+        todo1: maak een multidimensionale array met daarin alle checkins zoals te zien op screenshots/screenshot1.png
+            - denk na over welke data er in je array moet zitten
+            - soms voeg je een foto toe, soms niet (tip: gebruik voor je foto's pexels.com of een andere gratis leverancier)
+            - op screenshots/screenshot2.jpeg kan je zien wat bedoelt wordt met een checkin met foto
+            - werk met isset() of empty() om de foto soms wel en soms niet af te drukken
+
+
+        todo2: werk met een constant DISTANCE waarmee je kan instellen wat de maximale afstand is om checkins voor te tonen
+            - je zal in je array een extra stukje data moeten bijvoegen om deze afstand mee te betrekken in je checkins
+
+    */
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Swarm App</title>
+</head>
+<body>
+    <?php // todo3 : lus over je checkins en print deze visueel af zoals op de screenshots/screenshot1.png?>
+
+    <?php // todo4 : zorg dat je header en footer opgehaald wordt vanuit footer.inc.php en header.inc.php zodat je deze kan hergebruiken op meerdere schermen?>
+</body>
+</html>
